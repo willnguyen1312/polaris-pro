fb86915010c39142c040c960b5865ecb2085e2dd
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-21 10:16:24 -0300

    Let's go Polar 🐻‍❄️

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { AppProvider, Button, ButtonGroup } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
+import enTranslations from "@shopify/polaris/locales/en.json";
 
-export default App;
+export default function App() {
+  return (
+    <AppProvider i18n={enTranslations}>
+      <ButtonGroup gap="loose">
+        <Button
+          children="Hi Polaris 💞"
+          size="medium"
+          textAlign="center"
+          tone="success"
+          variant="primary"
+        />
+      </ButtonGroup>
+    </AppProvider>
+  );
+}
