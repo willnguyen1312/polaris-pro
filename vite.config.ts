821bdacb78be094497246bd9d429b775dcf9fde3
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-09 18:58:01 -0400

    Hmm. Minimal reprod 😅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { inspectReact } from "@namnode/vite-plugin-inspect-react";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    inspectReact({
+      formatDataInspectId(id) {
+        return id.substring(__dirname.length + 1);
+      },
+    }),
+    react(),
+  ],
 });
