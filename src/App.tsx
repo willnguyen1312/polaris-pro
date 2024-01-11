666c6eda619dd3dbaa81d6e78ba7019c2b5cd215
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-11 09:42:05 -0400

    Embrace the platform 🤗

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,57 @@
-// export { default } from "./DataTable";
-export { default } from "./IndexTable";
+import { ActionList, Button, Popover } from "@shopify/polaris";
+import { useCallback, useState } from "react";
+
+export default function App() {
+  return (
+    <>
+      <PopoverWithActionListExample />
+      <PopoverWithActionListExample />
+    </>
+  );
+}
+
+function PopoverWithActionListExample() {
+  const [popoverActive, setPopoverActive] = useState(false);
+
+  const togglePopoverActive = useCallback(
+    () => setPopoverActive((popoverActive) => !popoverActive),
+    []
+  );
+
+  const activator = (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        const event = new MouseEvent("click", {
+          bubbles: true,
+        });
+        window.dispatchEvent(event);
+      }}
+    >
+      <Button
+        onClick={() => {
+          togglePopoverActive();
+        }}
+        disclosure
+      >
+        More actions
+      </Button>
+    </div>
+  );
+
+  return (
+    <div style={{ height: "250px" }}>
+      <Popover
+        active={popoverActive}
+        activator={activator}
+        autofocusTarget="first-node"
+        onClose={togglePopoverActive}
+      >
+        <ActionList
+          actionRole="menuitem"
+          items={[{ content: "Import" }, { content: "Export" }]}
+        />
+      </Popover>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { inspectReact } from "@namnode/vite-plugin-inspect-react";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    inspectReact({
-      formatDataInspectId(id) {
-        return id.substring(__dirname.length + 1);
-      },
-    }),
+    // inspectReact({
+    //   formatDataInspectId(id) {
+    //     return id.substring(__dirname.length + 1);
+    //   },
+    // }),
     react(),
   ],
+  server: {
+    open: true,
+  },
 });
