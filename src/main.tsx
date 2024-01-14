b27b885d1a31db7f2a350e6e958771f5034983a0
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-14 10:53:35 -0400

    Add IndexTable example 💞

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./IndexTable";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
