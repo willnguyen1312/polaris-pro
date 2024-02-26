802d83b9ccfa828c88243f3f1863a5dd7f909f00
Nam Nguyen <nam.nguyen@shopify.com>
2024-02-26 10:47:51 -0400

    Add empty space example 🚀

---
+++ src/EmptyState.tsx
@@ -0,0 +1,21 @@
+import { Card, EmptyState } from "@shopify/polaris";
+
+function EmptyStateExample() {
+  return (
+    <Card>
+      <EmptyState
+        heading="Manage your inventory transfers"
+        action={{ content: "Add transfer" }}
+        secondaryAction={{
+          content: "Learn more",
+          url: window.location.href,
+        }}
+        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+      >
+        <p>Track and receive your incoming inventory from suppliers.</p>
+      </EmptyState>
+    </Card>
+  );
+}
+
+export default EmptyStateExample;
