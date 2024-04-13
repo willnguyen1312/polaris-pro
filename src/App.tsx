026726b95417f6714d602e67e5df52fb351dca09
Nam Nguyen <nam.nguyen@shopify.com>
2024-04-13 18:31:41 -0300

    Weirdo 🙈

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,120 @@
-import { ActionList, Box, Button, Popover } from "@shopify/polaris";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { faker } from "@faker-js/faker";
+import {
+  ActionListProps,
+  Box,
+  Button,
+  Checkbox,
+  Popover,
+} from "@shopify/polaris";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from "react";
 
-window.addEventListener("click", () => {
-  alert("window click");
-});
+const initialItems: ActionListProps["items"] = Array.from(
+  { length: 20 },
+  () => {
+    return {
+      content: faker.lorem.words(2),
+      id: faker.string.uuid(),
+    };
+  }
+);
 
-document.addEventListener("click", () => {
-  alert("document click");
+// Create a context with a true/false value
+const context = createContext({
+  checked: false,
+  items: [] as ActionListProps["items"],
+  onCheckboxChange: () => {},
 });
 
 export default function App() {
   const boxRef = useRef<HTMLElement>(null);
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    console.log("App mounted");
-
-    console.log("boxRef.current", boxRef.current);
-
-    console.log("buttonRef.current", buttonRef.current);
-  }, []);
+  const [value, setValue] = useState(true);
+  const [items, setItems] = useState<ActionListProps["items"]>(initialItems);
 
   return (
-    <div>
+    <context.Provider
+      value={{
+        checked: value,
+        items,
+        onCheckboxChange: () => {
+          setValue((value) => !value);
+          setItems((items: ActionListProps["items"] = []) => {
+            return items.map((item, index) => {
+              return {
+                id: item.id,
+                content: `${item.content} ${index}`,
+              };
+            });
+          });
+        },
+      }}
+    >
       <Box ref={boxRef}>
         <PopoverWithActionListExample />
-        <PopoverWithActionListExample />
       </Box>
-
-      {/* <Button ref={buttonRef}>Hi button</Button> */}
-    </div>
+    </context.Provider>
   );
 }
 
 function PopoverWithActionListExample() {
+  const { items, checked, onCheckboxChange } = useContext(context);
   const [popoverActive, setPopoverActive] = useState(false);
 
   const togglePopoverActive = useCallback(
     () => setPopoverActive((popoverActive) => !popoverActive),
-    [],
+    []
   );
 
   const activator = (
-    <div
-      onClick={(e) => {
-        e.stopPropagation();
-        window.dispatchEvent(new CustomEvent("click"));
+    <Button
+      onClick={() => {
+        togglePopoverActive();
       }}
+      disclosure
     >
-      <Button
-        onClick={() => {
-          togglePopoverActive();
-        }}
-        disclosure
-      >
-        More actions
-      </Button>
-    </div>
+      More actions
+    </Button>
   );
 
   return (
-    <div style={{ height: "250px" }}>
-      <Popover
-        active={popoverActive}
-        activator={activator}
-        autofocusTarget="first-node"
-        onClose={togglePopoverActive}
-      >
-        <ActionList
-          actionRole="menuitem"
-          items={[{ content: "Import" }, { content: "Export" }]}
+    <Popover
+      active={popoverActive}
+      activator={activator}
+      autofocusTarget="first-node"
+      onClose={togglePopoverActive}
+    >
+      <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+        <div
+          style={{
+            overflowY: "auto",
+            overscrollBehavior: "contain",
+          }}
+        >
+          {items?.map((item) => {
+            return (
+              <div
+                key={item.id}
+                style={{
+                  padding: "10px",
+                  borderBottom: "1px solid #ccc",
+                }}
+              >
+                {item.content}
+              </div>
+            );
+          })}
+        </div>
+        <Checkbox
+          label="Checkbox"
+          checked={checked}
+          onChange={onCheckboxChange}
         />
-      </Popover>
-    </div>
+      </div>
+    </Popover>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 // import ReactInspector from "vite-plugin-react-inspector";
-import { inspectReact } from "@namnode/vite-plugin-inspect-react";
 import Inspect from "vite-plugin-inspect";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Inspect(),
-    inspectReact({
-      base: __dirname,
-    }),
+    // inspectReact({
+    //   base: __dirname,
+    // }),
     // ReactInspector(),
     react(),
   ],
