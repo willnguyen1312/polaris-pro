4e1f5fff8c42073861542745f403e3c9f58771b0
Nam Nguyen <nam.nguyen@shopify.com>
2025-03-18 09:34:03 -0600

    xD

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { AppProvider } from "@shopify/polaris";
+import { AppProvider, Button } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import React from "react";
@@ -8,7 +8,18 @@ import ReactDOM from "react-dom/client";
 // import App from "./EmptyState";
 // import App from "./Optmized";
 // import App from "./Unoptimized";
-import App from "./IndexTable";
+// import App from "./IndexTable";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button loading onClick={() => alert("clicked")}>
+        Click me
+      </Button>
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
